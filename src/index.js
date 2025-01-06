2ba166a9f6dd69f5427e0301aeb5f221bbd78521
code-with-SHAHRUKH <138289094+code-with-SHAHRUKH@users.noreply.github.com>
2025-01-06 06:01:33 -0800

    How to Run Express Server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 
 import dotenv from 'dotenv';
-
+import app from './App.js';
 import ConnectDB from './db/index.js';
 dotenv.config({path:'./env'});
-ConnectDB()
+
+//ConnectDB give me promiss so we can use then with it---> we can also use async await
+ConnectDB().then(() => {
+    app.listen(process.env.PORT||8000,()=>{
+        console.log("Express Server is Running at port",process.env.PORT);
+    })
+}).catch((err) => {
+    console.log("Mongo DB connection failed:",err);
+});
 
 
 
