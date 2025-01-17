0be45d3c7190100f69b62e4b808eac610cb787d0
code-with-SHAHRUKH <138289094+code-with-SHAHRUKH@users.noreply.github.com>
2025-01-17 03:50:00 -0800

    How to write subpipelines to connect User Document with Watch history documet

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import {registerUser,loginUser,logoutUser,refreshAccessToken} from "../Controllers/user.controller.js";
+import {registerUser,
+        loginUser,
+        logoutUser,
+        refreshAccessToken,
+        changeCurrentPassword,
+        getCurrentUser,
+        updateAccountDetails,
+        updateUserAvatar,
+        updateUserCoverImage,
+        getUserChannelProfile,
+        getWatchHistory
+       } from "../Controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
@@ -22,8 +33,21 @@ router.route("/register").post(upload.fields(
 ), registerUser)
 
 router.route("/login").post(loginUser)
-//secured routes
+
+
+//    ---------------> Secured routes <---------------
 router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)// is me middle ware na de to bhi chale
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+//    --------------> get data from backend <---------------
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)//data url se backend me ja rha na k req.body se
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router
